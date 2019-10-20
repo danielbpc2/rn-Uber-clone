a49b55e491cb2ddd8a00bae148fa6865bbeb0e7a
Daniel Carvalho <danielbpc2@hotmail.com>
2019-10-19 22:30:20 -0300

    File Structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>Hello</Text>
+  </View>
+);
+
+export default App;
