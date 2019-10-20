3d20b4c6bc652fc1da25dd767312a07ec7278b4c
Daniel Carvalho <danielbpc2@hotmail.com>
2019-10-19 23:00:34 -0300

    Feat: Working Map

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+import MapView from 'react-native-maps';
 
 const App = () => (
-  <View>
-    <Text>Hello</Text>
+  <View style={{flex: 1}}>
+    <MapView
+      style={{flex: 1}}
+      region={{
+        latitude: -27.210753,
+        longitude: -49.644183,
+        latitudeDelta: 0.0143,
+        longitudeDelta: 0.0134,
+      }}
+      showsUserLocation
+      loadingEnabled
+    />
   </View>
 );
 
