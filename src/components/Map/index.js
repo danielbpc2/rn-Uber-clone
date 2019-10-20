f4c4f4673350c77b2c501a6464bef181c2b4155f
Daniel Carvalho <danielbpc2@hotmail.com>
2019-10-20 16:57:23 -0300

    Feat: auto complete on Search

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,20 +1,47 @@
 import React, {Component} from 'react';
 import MapView from 'react-native-maps';
+import {View} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import Search from '../Search';
 
 export default class Map extends Component {
+  state = {region: null};
+
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      ({coords: {latitude, longitude}}) => {
+        this.setState({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134,
+          },
+        });
+      }, //sucesso
+      error => {
+        console.log(error);
+      }, //erro
+      {
+        timeout: 2000,
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+      },
+    );
+  }
+
   render() {
+    const {region} = this.state;
     return (
-      <MapView
-        style={{flex: 1}}
-        region={{
-          latitude: -8.117809,
-          longitude: -34.897593,
-          latitudeDelta: 0.0143,
-          longitudeDelta: 0.0134,
-        }}
-        showsUserLocation
-        loadingEnabled
-      />
+      <View style={{flex: 1}}>
+        <MapView
+          style={{flex: 1}}
+          region={region}
+          showsUserLocation
+          loadingEnabled
+        />
+        <Search />
+      </View>
     );
   }
 }
